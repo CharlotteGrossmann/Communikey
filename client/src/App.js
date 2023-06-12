a6d38a4b3ff20ce82cad589dbdea007ade2e4a0c
CharlotteGrossmann <33022095+CharlotteGrossmann@users.noreply.github.com>
2023-06-12 22:03:51 +0200

    Add Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,100 @@
 import React, { useState, Fragment } from "react";
 import "./App.css";
- 
+import Heading from "./components/Header";
+import Button from "./components/MsgButton";
+
 var mqtt = require('mqtt');
 const username = 'Communikey';
 const password = 'talkToM3@h1vemq';
+var count = 0;
+var mymessage = "";
+var messages = ["Element 1", "element 2", "element 3", " element 4", "element 5"];
 
 var options = {
   username,
   password,
   retain: true,
-  clientId: "mqttjs01" ,
+  clientId: "mqttjs01",
   qos: 1
 };
 
 
-var client = mqtt.connect("wss://e392ddf2f8ae4c36b47cc789127d293e.s2.eu.hivemq.cloud:8884/mqtt",options) ;
+var client = mqtt.connect("wss://e392ddf2f8ae4c36b47cc789127d293e.s2.eu.hivemq.cloud:8884/mqtt", options);
 var topic = "Almeria22";
 
 client.subscribe(topic, { qos: 1 }); //single topic
 
 function App() {
   var note;
-  
+
   client.on("connect", function () {
     console.log("connected  " + client.connected);
 
-})
+  })
   client.on("message", function (topic, message) {
     note = message.toString();
     // Updates React state with message
-    setMsg(note);
-    console.log(note);
-    
-  });
- 
-  // Sets default React state
-  const [msg, setMsg] = useState(
-    <Fragment>
-      <em>...</em>
-    </Fragment>
-  );
- 
+    messages.push(note);
+    messages.shift();
 
+  });
+
+ const [messages, setMessages]= useState(["Element 1", "element 2", "element 3", " element 4", "element 5"]);
+
+  function publish(topic, msg, options) {
+
+
+    if (client.connected == true) {
+
+      client.publish(topic, msg, options);
+
+    }
+    count += 1;
+    if (count == 2) //ens script
+      clearTimeout(timer_id); //stop timer
+    client.end();
+  }
+  var timer_id = setInterval(function () { publish(topic, mymessage, options); }, 5000);
+
+  function push(){
+    messages.push("lol");
+    messages.shift();
+    setMessages(messages);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello MQTT in React</h1>
-        <p>The message payload is: {msg}</p>
-      </header>
+
+    <div>
+      <section id="Header">
+        <Heading />
+      </section>
+
+      <section id="outgoing">
+        <div className="box" id="Messaging">
+          <Button onClick={push} text="senden" />
+        </div>
+        <div className="box" id="Chakras">
+          <canvas>
+
+          </canvas>
+          <Button text="speichern" />
+        </div>
+      </section>
+
+      <section className="box" id="Feed">
+        <div>
+          <ul>
+            {messages.map((element, index) => (<li key={index}>{element}</li>))}
+            
+          </ul>
+        </div>
+      </section>
+
+
+
+
     </div>
+
   );
 }
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <div>
+      <h1>Communikey</h1>
+    </div>
+  )
+}
+
+export default Heading
